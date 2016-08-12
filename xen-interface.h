0be10e2fa14b0c0689d1976d34daf9e762895a23
Florian Schmidt <florian.schmidt@neclab.eu>
2016-08-12 12:26:01 +0000

    arm-support: don't hold you breath for libxc support
    Since libxc has support for memory translation for x86, but not arm,
    there is surrently no way for libxc to support tracing on arm. I might
    get around to write a libxc version of my page traversal function once I
    have a fully working version of it for libxencall... but don't wait with
    bated breath.

--- xen-interface.h
+++ xen-interface.h
@@ -10,6 +10,10 @@
 #warning You defined more than one of HYPERCALL_LIBXC, HYPERCALL_XENCALL. This might lead to unexpected results.
 #endif
 
+#if defined(__arm__) && defined(HYPERCALL_LIBXC)
+//#error libxc does not support start tracing on arm
+#endif
+
 #if defined(HYPERCALL_XENCALL)
 #include <xenctrl.h>
 #include <xencall.h>
