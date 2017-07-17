sysml/uniprof

commit 9c05135ead65ce46ff098f1f20659d2b8525d443

Do not wait for another interval after the last stack walk
After the last requested stackwalk (i.e., after time*freq walks), do not
wait around another time step (which can be up to one second). This
makes uniprof feel slightly more responsive on quick one-shot "just give
me a single stack trace" usage. It also makes a world of difference for
automated testing.

uniprof.c
@@ -631,7 +631,8 @@ int main(int argc, char **argv) {
 					fprintf(stderr, "we're falling behind by %ld.%09ld!\n", ts.tv_sec, ts.tv_nsec);
 				}
 			}
-			else {
+			else if ( (j < (freq-1)) && (i < (time-1)) )  {
+				// only sleep if it's not the very last round
 				timespecsub(&ts, &end, &ts);
 				if (timespeccmp(&ts, &minsleep, <)) {
 					// we finished so close to the next deadline that nanosleep() cannot

commit 34628eeef504c166cd7c6852a85f245819174103

Fix bug introduced with 9c05135
What was supposed to only skip sleeping after the last stack trace
instead skipped sleeping on the last stack trace of every second, and
all sleeps on the last second. Whoops.

uniprof.c
@@ -655,7 +655,7 @@ int main(int argc, char **argv) {
 					fprintf(stderr, "we're falling behind by %ld.%09ld!\n", ts.tv_sec, ts.tv_nsec);
 				}
 			}
-			else if ( (j < (freq-1)) && (i < (time-1)) )  {
+			else if ( (i < (time-1)) || (j < (freq-1)) )  {
 				// only sleep if it's not the very last round
 				timespecsub(&ts, &end, &ts);
 				if (timespeccmp(&ts, &minsleep, <)) {